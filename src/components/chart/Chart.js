74a11816fe9eef03ae48f6a2e52453518b443867
Nicolas Chapron <nicolas.chapron2022@gmail.com>
2022-03-29 12:13:59 +0100

    Finalise Chart

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -1,16 +1,21 @@
 import React from "react";
-
-import "./Chart.css";
 import ChartBar from "./ChartBar";
 
+import "./Chart.css";
+
 const Chart = (props) => {
+  // Add each datapoints values to an array dataPointsValues which is just an array of numbers. Here 12 months so 12 values
+  const dataPointsValues = props.dataPoints.map((dataPoint) => dataPoint.value); // transform object to number
+  // dataPointsValues is still an array so I pull out the elements with the spread operator to add them as a standalone argument
+  const totalMaximum = Math.max(...dataPointsValues);
+
   return (
     <div className="chart">
-      {props.datPoints.map((dataPoint) => (
+      {props.dataPoints.map((dataPoint) => (
         <ChartBar
           key={dataPoint.label}
           value={dataPoint.value}
-          maxValue={null}
+          maxValue={totalMaximum}
           label={dataPoint.label}
         />
       ))}
--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 import "./Expenses.css";
 
@@ -16,13 +17,13 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
