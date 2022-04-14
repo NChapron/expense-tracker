86664ab8f1fd92c68989a90b0090106ecee6fbcb
Nicolas Chapron <nicolas.chapron2022@gmail.com>
2022-04-14 18:24:19 +0100

    expenses update

--- src/App.js
+++ src/App.js
@@ -6,19 +6,31 @@ import NewExpense from "./components/newExpense/NewExpense";
 const DUMMY_EXPENSES = [
   {
     id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
+    title: "Rent",
+    amount: 1500,
+    date: new Date(2020, 1, 1),
   },
-  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e2",
+    title: "Car Insurance",
+    amount: 800.89,
+    date: new Date(2020, 3, 15),
+  },
   {
     id: "e3",
+    title: "gym",
+    amount: 289.89,
+    date: new Date(2020, 8, 22),
+  },
+  { id: "e4", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e5",
     title: "Car Insurance",
     amount: 294.67,
     date: new Date(2021, 2, 28),
   },
   {
-    id: "e4",
+    id: "e6",
     title: "New Desk (Wooden)",
     amount: 450,
     date: new Date(2021, 5, 12),
